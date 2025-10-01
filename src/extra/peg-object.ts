03c183ca1257adaefcb36ce171bf2bb6b649ad5e
aelafifi <ahmed.s.elafifi@gmail.com>
2025-10-01 09:37:04 +0300

    Add PEG object parser

--- src/extra/peg-object.ts
+++ src/extra/peg-object.ts
@@ -1,86 +1,93 @@
 import {
-  And,
+  and,
+  choice,
   EOF,
-  Keyword,
-  Not,
-  Optional,
-  RegexMatch,
-  StringMatch,
+  Epsilon,
+  keyword,
+  match$,
+  not,
+  oneOrMore,
+  optional,
+  ParsingExpression,
+  regexMatch,
+  sequence,
+  stringMatch,
+  zeroOrMore,
+  expression,
+  repetition,
+  match,
 } from "../parsing-expression";
-import { Parser } from "../parser";
-import { PTVisitor } from "../parse-tree/PTVisitor";
+import { DEFAULT_WS, Parser } from "../parser";
+import { Node } from "../parse-tree";
 import { GrammarDef } from "../types";
-import { PTNode } from "../parse-tree";
-import { OneOrMore } from "../parsing-expression/repetition/OneOrMore";
-import { ZeroOrMore } from "../parsing-expression/repetition/ZeroOrMore";
-import { SUPPRESS } from "../parsing-expression/constants";
-import { ParsingExpression } from "../parsing-expression/basic/ParsingExpression";
-import { Sequence } from "../parsing-expression/basic/Sequence";
-import { Choice } from "../parsing-expression/basic/Choice";
 
-const S_ = (str: string | RegExp) =>
-  str instanceof RegExp
-    ? new RegexMatch(str, { refiner: SUPPRESS })
-    : new StringMatch(str, { refiner: SUPPRESS });
+const choiceLine = () => [_choice, EOF];
 
-const choiceLine = () => new Sequence([choice, EOF], { refiner: 0 });
+const sequenceLine = () => [_sequence, EOF];
 
-const sequenceLine = () => new Sequence([sequence, EOF], { refiner: 0 });
+const _choice = () =>
+  oneOrMore(_sequence, { sep: match$("|"), ruleName: "choice" });
 
-const choice = () =>
-  new OneOrMore(sequence, {
-    sep: S_("|"),
-    refiner: (value) => (value.length === 1 ? value[0] : new Choice(value)),
-  });
+const _sequence = () => oneOrMore(FullExpression, { ruleName: "sequence" });
 
-const withSeparator = () =>
-  new Optional(["/", FullExpression], { refiner: (value) => value?.[1] });
+// TODO: allow repRange max to infinity
+const repRange = [
+  match$("{"),
+  regexMatch(/\d+/, {
+    refiner: (node: Node, value: string) => parseInt(value),
+  }),
+  optional([
+    match$(","),
+    regexMatch(/\d+/, {
+      refiner: (node: Node, value: string) => parseInt(value),
+    }),
+  ]),
+  match$("}"),
+];
 
+// TODO: Support `Partial` and `Repetition` types in PEGObject
 const FullExpression = () =>
-  new Sequence(
+  sequence(
     [
-      new Optional(new Choice(["&", "!", "#"])),
-      expression,
-      new Optional(
-        new Choice(["?", ["*", withSeparator], ["+", withSeparator]]),
-      ),
+      optional(choice(["&", "!", "#"])),
+      _expression,
+      optional(choice(["?", [choice(["*", "+", repRange]), withSeparator]])),
     ],
-    { skipws: "" },
+    {
+      skipws: "",
+      ruleName: "full_expression",
+    },
   );
 
-const sequence = () =>
-  new OneOrMore(FullExpression, {
-    refiner: (value) => (value.length === 1 ? value[0] : new Sequence(value)),
-  });
+const withSeparator = () => optional([match$("/"), FullExpression]);
 
-const expression = () =>
-  new Choice([
+const _expression = () =>
+  choice([
+    match("EOF", { refiner: () => EOF }),
+    RuleName,
     regex,
     str_match,
     keyword_match,
-    RuleName,
-    new Sequence(["(", choice, ")"], { refiner: 1 }),
+    sequence([match$("("), _choice, match$(")")], { skipws: DEFAULT_WS }),
   ]);
 
 // PEG Lexical rules
-const regex = () =>
-  new RegexMatch(/\/(?:\\.|\[[^\]]+]|.)*?\/[gimsuy]*/, {
-    refiner(value) {
-      const match = /^\/(.*)\/(.*)$/.exec(value);
-      return new RegexMatch(new RegExp(match![1], match![2]));
-    },
-  });
+const regex = regexMatch(/\/(?:\\.|\[[^\]]+]|.)*?\/[gimsuy]*/, {
+  refiner(node: Node, value: string) {
+    const match = /^\/(.*)\/(.*)$/.exec(value);
+    return regexMatch(new RegExp(match![1], match![2]));
+  },
+});
 
-const RuleName = () => /[a-zA-Z_]([a-zA-Z_]|[0-9])*\b/;
+const RuleName = () => /[a-zA-Z_]\w*\b/;
 
-const str_match = () =>
-  new RegexMatch(/"(?:\\[^\n]|[^\n"])*?"|'(?:\\[^\n]|[^\n'])*?'/, {
-    refiner: (value) => new StringMatch(value.slice(1, -1)),
-  });
+const str_match = regexMatch(/"(?:\\[^\n]|[^\n"])*?"|'(?:\\[^\n]|[^\n'])*?'/, {
+  refiner: (node: Node, value: string) => stringMatch(value.slice(1, -1)),
+});
 
 const keyword_match = () =>
-  new RegexMatch(/k"(?:\\[^\n]|[^\n"])*?"|k'(?:\\[^\n]|[^\n'])*?'/, {
-    refiner: (value) => new Keyword(value.slice(2, -1)),
+  regexMatch(/k"(?:\\[^\n]|[^\n"])*?"|k'(?:\\[^\n]|[^\n'])*?'/, {
+    refiner: (node: Node, value: string) => keyword(value.slice(2, -1)),
   });
 
 export type PEGObject = Record<
@@ -88,63 +95,73 @@ export type PEGObject = Record<
   RegExp | ParsingExpression | string | string[]
 >;
 
-export const Str = (str: string) => new StringMatch(str);
-
 function parsePEGObject(obj: PEGObject): any {
   const rules: Record<string, GrammarDef> = {};
-  const ruleRefGetter = (rule_name: string) => () => rules[rule_name];
+  const parser = new Parser(null);
 
-  const _parse = (value: string, rule: GrammarDef) => {
-    const pTree = Parser.parse(value, rule);
-    return new PTVisitor(pTree, {
-      afterRuleName(node: PTNode, value: string) {
-        if (value === "EOF") {
-          return EOF;
+  const _parse = (value: string, rule: GrammarDef) =>
+    parser.parse(value, rule).visit({
+      choice: (node: Node, value: any[]) =>
+        value.length > 1 ? choice(value) : value[0],
+
+      sequence: (node: Node, value: any[]) =>
+        value.length > 1 ? sequence(value) : value[0],
+
+      full_expression: (node: Node, [pre, expr, post]) => {
+        // Auto `suppress` if matched value is suppressed
+        if (expr === null) {
+          return null;
         }
-        return ruleRefGetter(value);
+
+        const suppress = pre === "#";
+        const _id = (rule: GrammarDef, ...args: any[]) => rule;
+        const _syntaxPredicate =
+          pre === "&" ? and : pre === "!" ? not : expression;
+        let _repetition = post === "?" ? optional : _id;
+        const opts: any = {};
+        if (Array.isArray(post)) {
+          if (post[0] === "*") {
+            _repetition = zeroOrMore;
+            opts.sep = post[1] ?? undefined;
+          } else if (post[0] === "+") {
+            _repetition = oneOrMore;
+            opts.sep = post[1] ?? undefined;
+          } else if (Array.isArray(post[0])) {
+            let [min, max] = post[0];
+            max = max ?? min;
+            _repetition = repetition;
+            opts.min = min;
+            opts.max = max;
+            opts.sep = post[1] ?? undefined;
+          }
+        }
+
+        return _syntaxPredicate(_repetition(expr, opts), { suppress });
       },
-      afterFullExpression(node: PTNode, [pre, expr, post]: any) {
-        const refiner = pre === "#" ? SUPPRESS : undefined;
-        const syntaxPredicate = pre === "&" ? And : pre === "!" ? Not : null;
-        const repetition =
-          post === "?"
-            ? Optional
-            : post?.[0] === "*"
-              ? ZeroOrMore
-              : post?.[0] === "+"
-                ? OneOrMore
-                : null;
 
-        const _sep = post?.[1] ?? undefined;
+      RuleName: (node: Node, value: string) => () => rules[value],
+    });
 
-        if (repetition) {
-          expr = new repetition(expr, { sep: _sep });
-        } else {
-          expr = new Sequence([expr]);
-        }
-        if (syntaxPredicate) {
-          expr = new syntaxPredicate(expr);
-        }
-        expr.refiner = refiner;
-        return expr;
-      },
-    }).visit();
-  };
-
-  const KEY_REGEX = /^[a-zA-Z_]([a-zA-Z_]|[0-9])*$/;
+  const KEY_REGEX = /^[a-zA-Z_]\w*$/;
   for (const [key, value] of Object.entries(obj)) {
     if (!KEY_REGEX.test(key)) {
       throw new Error(`Invalid key: ${key}`);
     }
 
+    if (["EOF"].includes(key)) {
+      throw new Error(`Key cannot be a reserved word: ${key}`);
+    }
+
     if (value instanceof RegExp) {
-      rules[key] = new RegexMatch(value);
+      rules[key] = regexMatch(value);
     } else if (value instanceof ParsingExpression) {
       rules[key] = value;
     } else if (typeof value === "string") {
       rules[key] = _parse(value, choiceLine);
+      (rules[key] as ParsingExpression).ruleName = key;
     } else if (Array.isArray(value)) {
-      rules[key] = new Choice(value.map((x) => _parse(x, sequenceLine)));
+      rules[key] = choice(value.map((x) => _parse(x, sequenceLine)));
+      (rules[key] as ParsingExpression).ruleName = key;
     } else {
       throw new Error(`Invalid value for key: ${key}`);
     }
@@ -154,18 +171,22 @@ function parsePEGObject(obj: PEGObject): any {
 }
 
 const peg: PEGObject = {
-  number: /\d*\.\d*|\d+/,
-  factor: '#("+" | "-")? (number | "(" expression ")")',
-  term: 'factor (("*" | "/" | "%") factor)*',
-  expression: 'term (("+" | "-") term)*',
-  calc: "expression EOF",
+  // number: /\d*\.\d*|\d+/,
+  // factor: '#("+" | "-")? (number | "(" expression ")")',
+  // term: 'factor (("*" | "/" | "%") factor)*',
+  // expression: 'term (("+" | "-") term)*',
+  // calc: "expression EOF",
   b: "'x'",
-  a: "b+/#','",
+  a: "b{2,5}/#',' EOF",
 
-  x: "'x'?",
-  y: "x+",
+  // x: "'x'?",
+  // y: "x+ | '12'",
 };
 
-console.log(Parser.parse("(1 + 2) * 5", parsePEGObject(peg).calc).refined);
-console.log(Parser.parse("x, x, x", parsePEGObject(peg).a).refined);
-console.log(Parser.parse("", parsePEGObject(peg).y).refined);
+// console.log(parsePEGObject(peg).a.elements);
+
+console.log(
+  // new Parser().parse("(1 + 2) * 5", parsePEGObject(peg).calc).treeStr(),
+  new Parser().parse("x, x, x", parsePEGObject(peg).a).value,
+);
+// console.log(new Parser().parse("", parsePEGObject(peg).y).value);
